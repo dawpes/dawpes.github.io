98f7153b15b29970e86eb9a317bc1d440f9083ab
dawpes <dawpes1984@gmail.com>
2016-12-15 16:30:37 +0100

    Update maps.js

--- maps.js
+++ maps.js
@@ -1,4 +1,4 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+var mymap = L.map('mapid').setView([50.29889, 18.95378], 13); 
 
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
 		maxZoom: 18,
